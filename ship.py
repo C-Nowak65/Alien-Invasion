22e81af189ed76e521a26ae1a4bf3aeff3c25b85
chno <ctnowak65@gmail.com>
2023-11-12 11:51:25 -0500

    [IMP] In this commit there are a number of new features introduced. These features include responding to keypresses, allowing continuous movement (left and right), adjusting the ship's  speed, ensuring the ship does not leave the screen, pressing q to quit, running the game in fullscreen, and refactoring _check_events.

--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ class Ship:
     '''A class to manage the ship.'''
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #load the ship
@@ -12,6 +13,23 @@ class Ship:
 
         #Start each new ship at the bottom center of the ship
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #store a float for the ships exact horizontal position
+        self.x = float(self.rect.x)
+
+        #movement flag: start with a ship that is not moving
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        '''Update the ship's postion based on movement flags'''
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        #update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         '''Draw the ship at its current location.'''
